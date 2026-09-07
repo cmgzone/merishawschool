93a5666becd9812f55d799552cec33cec85585d0
cmgmerlin <cmgmerlin@users.noreply.github.com>
2026-09-07 14:52:53 +0300

    Update fee downloads and remove 2023 notices

--- data/downloads.ts
+++ data/downloads.ts
@@ -1,7 +1,7 @@
 export const downloads = [
   {
-    title: "Current Fee Structure",
-    description: "Current school fee structure for parent review.",
+    title: "2026 Fee Structure",
+    description: "2026 school fee structure for parent review.",
     href: "/docs/merishaw-current-fee-structure.pdf",
     type: "PDF",
     isLocal: true,
@@ -10,20 +10,6 @@ export const downloads = [
     title: "School Brochure",
     description: "School brochure with an overview of the Merishaw experience.",
     href: "https://drive.google.com/file/d/1z_HivOAx812hqgenWXXhiTAQvcMU3TYr/view?usp=sharing",
-    type: "Google Drive",
-    needsClientApproval: true,
-  },
-  {
-    title: "End of Term 1 Circular 2023",
-    description: "Archived circular available for reference.",
-    href: "https://drive.google.com/file/d/1BQ_156oRsozf-U-X5anE2uypiJiulkmf/view?usp=sharing",
-    type: "Google Drive",
-    needsClientApproval: true,
-  },
-  {
-    title: "2023 Fee Structure",
-    description: "Previous fee structure available for reference.",
-    href: "https://drive.google.com/file/d/1DaEbrJixYInUzbWV5HHXSzfq40Ic1BPF/view?usp=sharing",
     type: "Google Drive",
     needsClientApproval: true,
   },
